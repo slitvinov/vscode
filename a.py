9367f62e035c5604c961d30353126a7886a54664
Sergey Litvinov <slitvinov@gmail.com>
2021-03-14 19:06:28 +0100

    add save()

--- a.py
+++ a.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
-from base64 import b64encode
+import base64
 import io
+def save(path):
+    f = io.BytesIO()
+    with open(path, "w") as file:
+        for i, num in enumerate(plt.get_fignums()):
+            f.seek(0)
+            plt.figure(num).savefig(f)
+            f.seek(0)
+            s = base64.b64encode(f.read()).decode()
+            file.write('<img src="data:image/png;base64,%s"/>\n' % s)
+            if i % 2 == 1:
+                file.write("</br>\n")
 
-f = io.BytesIO()
-plt.plot([1, 2, 3, 4, 5], 'o-')
-plt.savefig(f)
+plt.figure()
+plt.plot([1, 2, 3, 4, 5], 'r-')
 
-f.seek(0)
-s = b64encode(f.read()).decode()
-with open("a.md", "w") as f:
-    f.write('<img src="data:image/png;base64,%s"/>' % s)
+plt.figure()
+plt.plot([10, 2, 30, 4, 50], 'b-')
+
+plt.figure()
+plt.plot([10, 2, 30, 4, 50], 'g-')
+
+plt.figure()
+plt.plot([10, 2, 12, 6, 50], 'm-')
+
+save("a.md")

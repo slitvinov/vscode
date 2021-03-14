3f439d4a55f9ccc1bf90ee2718c529beb51c24a4
Sergey Litvinov <slitvinov@gmail.com>
2021-03-14 19:10:53 +0100

    add save() counts figures

--- a.py
+++ a.py
@@ -5,15 +5,18 @@ import base64
 import io
 def save(path):
     f = io.BytesIO()
+    cnt = 0
     with open(path, "w") as file:
-        for i, num in enumerate(plt.get_fignums()):
+        for num in plt.get_fignums():
             f.seek(0)
             plt.figure(num).savefig(f)
             f.seek(0)
             s = base64.b64encode(f.read()).decode()
             file.write('<img src="data:image/png;base64,%s"/>\n' % s)
-            if i % 2 == 1:
+            cnt += 1
+            if cnt % 2 == 0:
                 file.write("</br>\n")
+    return cnt
 
 plt.figure()
 plt.plot([1, 2, 3, 4, 5], 'r-')
@@ -27,4 +30,4 @@ plt.plot([10, 2, 30, 4, 50], 'g-')
 plt.figure()
 plt.plot([10, 2, 12, 6, 50], 'm-')
 
-save("a.md")
+print(save("a.md"))
